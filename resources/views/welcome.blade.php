3c9f0778ce738b37826f9a2d236f6d2b544862b3
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2024-01-22 17:57:50 +0000

    update welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 </head>
 <body class="antialiased bg-[#4a2d56]">
 <div class="h-screen w-full flex flex-col items-center justify-center">
-    <img src="https://peacockmarketing.co.uk/app/default/assets/addons/default/hup234design/peacock-theme/resources/img/logos/Peacock-Marketing-Logo-White-High.png?v=1611058637" class="max-w-3/4">
+    <img src="https://peacockmarketing.co.uk/app/default/assets/addons/default/hup234design/peacock-theme/resources/img/logos/Peacock-Marketing-Logo-White-High.png?v=1611058637" class="w-[640px] max-w-3/4">
 </div>
 </body>
 </html>
